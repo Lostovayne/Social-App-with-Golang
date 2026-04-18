23054e39d49744b9f1e9399a53aac54f9c224727
Deus lo Vult <moises.barrera.dev@gmail.com>
2026-04-17 22:44:29 -0400

    Implement getPostHandler and update storage interface for post retrieval

--- cmd/api/json.go
+++ cmd/api/json.go
@@ -5,12 +5,14 @@ import (
 	"net/http"
 )
 
+// writeJson se encarga de escribir la respuesta en formato JSON. Recibe el status code y los datos a enviar, establece el header Content-Type a application/json, escribe el status code y codifica los datos en JSON para enviarlos al cliente.
 func writeJson(w http.ResponseWriter, status int, data any) error {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	return json.NewEncoder(w).Encode(data)
 }
 
+// readJson se encarga de leer el JSON del body de la petición y decodificarlo en la estructura que le pasemos como argumento. Además, limita el tamaño del body para evitar ataques de denegación de servicio (DoS) y deshabilita la decodificación de campos desconocidos para evitar errores silenciosos.
 func readJson(w http.ResponseWriter, r *http.Request, data any) error {
 	// Block Atacks
 	maxBytes := 1_048_576 // 1MB
@@ -22,6 +24,7 @@ func readJson(w http.ResponseWriter, r *http.Request, data any) error {
 
 }
 
+// writeJsonError es una función auxiliar que se encarga de enviar un mensaje de error en formato JSON. Recibe el status code y el mensaje de error, crea una estructura con el campo "error" y llama a writeJson para enviar la respuesta al cliente.
 func writeJsonError(w http.ResponseWriter, status int, message string) error {
 
 	type envelope struct {
--- cmd/api/posts.go
+++ cmd/api/posts.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"errors"
 	"net/http"
+	"strconv"
 
 	"github.com/Elevate-Techworks/social/internal/store"
+	"github.com/go-chi/chi/v5"
 )
 
 type CreatePostPayload struct {
@@ -35,6 +38,35 @@ func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request
 	}
 
 	if err := writeJson(w, http.StatusCreated, post); err != nil {
+		writeJsonError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+}
+
+func (app *application) getPostHandler(w http.ResponseWriter, r *http.Request) {
+	// Obtenemos el postID de la URL y lo convertimos a int64
+	postId, err := strconv.ParseInt(chi.URLParam(r, "postID"), 10, 64)
+	ctx := r.Context()
+
+	if err != nil {
+		writeJsonError(w, http.StatusBadRequest, "Invalid post ID")
+		return
+	}
+
+	post, err := app.store.Posts.GetByID(ctx, postId)
+
+	if err != nil {
+		switch {
+		case errors.Is(err, store.ErrNotFound):
+			writeJsonError(w, http.StatusNotFound, err.Error())
+		default:
+			writeJsonError(w, http.StatusInternalServerError, err.Error())
+		}
+		return
+	}
+
+	if err := writeJson(w, http.StatusOK, post); err != nil {
 		writeJsonError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
